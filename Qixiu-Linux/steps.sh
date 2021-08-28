f3b6a88954d3203891bb1c6015333aaa6cb84484
linlinger <32541118+linlinger@users.noreply.github.com>
2021-08-28 17:59:57 +0800

    Update steps.sh
    Enable Bluetooth service

--- Qixiu-Linux/steps.sh
+++ Qixiu-Linux/steps.sh
@@ -120,8 +120,11 @@ ln -sf /usr/lib/systemd/system/NetworkManager-dispatcher.service ./releng/airoot
 ln -sf /usr/lib/systemd/system/localegen.service ./releng/airootfs/etc/systemd/system/multi-user.target.wants/localegen.service
 ln -sf /usr/lib/systemd/system/sddm.service ./releng/airootfs/etc/systemd/system/display-manager.service
 ln -sf /usr/lib/systemd/system/haveged.service ./releng/airootfs/etc/systemd/system/sysinit.target.wants/haveged.service
+#Enable Bluetooth Service
+mkdir ./releng/airootfs/etc/systemd/system/bluetooth.target.wants/
 ln -sf /usr/lib/systemd/system/bluetooth.service ./releng/airootfs/etc/systemd/system/dbus-org.bluez.service
 ln -sf /usr/lib/systemd/system/bluetooth.service ./releng/airootfs/etc/systemd/system/bluetooth.target.wants/bluetooth.service
+
 }
 
 # Copy files to customize the ISO
